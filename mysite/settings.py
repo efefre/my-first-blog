cea276011af7ce6d80faa0bd4b616b6293d1dd28
efefre <efefre@iloud.com>
2017-05-28 13:45:38 +0200

    add 'efefre.pythonanywhere.com' to ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+tb*+u8invi8b&$75rdu3bhe@!%##k1bqjrh-yqj)%62&yqgy*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['efefre.pythonanywhere.com']
 
 
 # Application definition
